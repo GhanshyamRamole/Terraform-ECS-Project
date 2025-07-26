4fdbfb8a1c493daa7fa9da8ca33b0a7012943276
Ubuntu <ubuntu@ip-172-31-14-100.ap-south-1.compute.internal>
2025-07-26 07:46:55 +0000

    main.tf

--- main.tf
+++ main.tf
@@ -2,18 +2,10 @@ provider "aws" {
   region = var.aws_region
 }
 
-module "vpc" {
-  source     = "./modules/vpc"
-  vpc_cidr   = var.vpc_cidr
-  aws_region = var.aws_region
-}
-
-module "iam" {
-  source = "./modules/iam"
-}
-
-module "ecs" {
-  source              = "./modules/ecs"
+module "project" {
+  source              = "./modules/"
+  vpc_cidr   	      = var.vpc_cidr
+  aws_region	      = var.aws_region
   cluster_name        = var.cluster_name
   ecr_image_url       = var.ecr_image_url
   subnets             = module.vpc.public_subnets
